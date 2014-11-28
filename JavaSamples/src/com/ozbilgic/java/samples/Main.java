e265cd9bee44910f20fc5de762389a6ed462aa2f
ozbilgic <ozbilgiclevent@gmail.com>
2014-11-28 23:49:49 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	JavaSamples/.idea/workspace.xml
    	JavaSamples/src/com/ozbilgic/java/samples/Main.java

--- JavaSamples/src/com/ozbilgic/java/samples/Main.java
+++ JavaSamples/src/com/ozbilgic/java/samples/Main.java
@@ -1,10 +1,18 @@
+/**
+ * Bu sinifta test amacli kodlar kullanilicaktir.
+ */
 package com.ozbilgic.java.samples;
 
 /**
- * Created ozbilgic on 19.11.2014 - 10:40.
+ * @author ozbilgic
+ * @since 19.11.2014
+ * @version 0.0.2
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Integer i0 = 0; //sarmalayici sinif atamasi. "i0" nesnesine bircok ozellik kazandirir.
+        int i1 = 0; //ilkel tip atamasi. Sadece veriyi okuma ve yazma icin.
     }
 }
